1917f77e98d24d098b6cc690bc1ca66bd88da483
Laurentiu Stoleriu <lstoler@staff.uaic.ro>
2023-03-23 16:51:13 +0200

    pthreads_1_serial

---
+++ p1_threads.c
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int count = 0;
+int* createArray(int size, int numai5);
+void numara5Serial(int *array, int size);
+
+int main() {
+    int size = 10000;
+    int *array = createArray(size, 1);
+    count = 0;
+    numara5Serial(array, size);
+    printf("Avem %d elemente egale cu 5 in array\n", count);
+    free(array);
+    return EXIT_SUCCESS;
+}
+
+int* createArray(int size, int numai5) {
+    srand(time(NULL));
+    int *array = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        int r = rand();
+        if (numai5) {
+            array[i] = 5;
+        }
+        else {
+            array[i] = (r % 10) + 1;
+        }
+    }
+    return array;
+}
+
+void numara5Serial(int *array, int size) 
+{
+    for (int i = 0; i < size; i++) 
+    {
+        if (5 == array[i]) // Yoda script
+        {
+            count++;
+        }
+    }
+}
+
